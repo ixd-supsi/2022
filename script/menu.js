6253ab0e89e0842b3d8c8bf31f84d21ee1430c29
giovannipro <g.profet@gmail.com>
2023-03-02 14:25:04 +0100

    add menu

---
+++ script/menu.js
@@ -0,0 +1,20 @@
+function load_menu(){
+    const container = document.getElementById("menu");
+
+    const pages = [
+        {
+            "link": "2022",
+        },
+        {
+            "link": "2023"
+        }
+    ]
+
+    for (let x = 0; x < pages.length; x++){
+        container.innerHTML += "<li><a class='link' href=https://ixd-supsi.github.io/" + pages[x].link + ">" + pages[x].link +" </a></li>"
+    }                
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    load_menu()
+});
